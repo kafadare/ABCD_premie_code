45e5a446ee5cef89338fc777c84e8bc0f8d38f73
Kafadar <kafadare@reslnvhpc0361.research.chop.edu>
2024-09-03 09:49:38 -0400

    submission scripts to run a specified model by inputting model directory and array task id with the gamlss_model_fit.R script. Currently running alternative abcd_gamlss_model_fit_CG.R script, this was a quick and dirty let's run the CG algorithm test, in the future will adapt the OG script to include the algorithm (RSE vs CG as an argument)

--- submit_gamlss_fits_ABCD.sh
+++ submit_gamlss_fits_ABCD.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=ABCD_family_selection_test
-#SBATCH --output=%x_%A_%a_output.txt
-#SBATCH --time=01:00:00
+#SBATCH --output=/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/out_messages/%x_%A_%a_output.txt
+#SBATCH --time=04:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
 
 # Directory containing the model files
-MODEL_DIR="~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_models/"
+MODEL_DIR="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_family_v2/"
+DATA_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/CSV/process_tables_5.1/abcd5.1_long_selectVars_NOdxfilter_famfilter2024-07-15.csv"
+OUT_FOLDER="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/"
+OUTPUT_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/gamlss_family_fits_stats_CG.csv"
 
-# Get the list of model files
-model_files=($(ls $MODEL_DIR))
-
-# Get the Nth file based on the array index
-MODEL_NAME=${model_files[$SLURM_ARRAY_TASK_ID - 1]}
-
-echo "Model name: $MODEL_NAME"
-
-OUTPUT_DIR="~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/out_messages/"
-ERROR_DIR="~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/err_messages/"
-
-# Specify the output and error file names based on the model name
-#SBATCH --output=${OUTPUT_DIR}${MODEL_NAME}_output.txt
-#SBATCH --error=${ERROR_DIR}${MODEL_NAME}_error.txt
-  
-DATA_FILENAME="~/Documents/Grad_School/BGDLab/ABCD_data/abcd5.1_long_selectVars_NOdxfilter_famfilter2024-07-04.csv"
-OUT_FOLDER="~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/"
-OUTPUT_FILENAME="~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/gamlss_fits_stats.csv"
+module load R/4.2.3
 
 # Run the R script with the specified model
-Rscript abcd_gamlss_model_fit.R $MODEL_NAME $DATA_FILENAME $OUT_FOLDER $OUTPUT_FILENAME
+Rscript abcd_gamlss_model_fit_CG.R $SLURM_ARRAY_TASK_ID $MODEL_DIR $DATA_FILENAME $OUT_FOLDER $OUTPUT_FILENAME
