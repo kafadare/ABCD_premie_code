a294397f964b555ad1e7ed86d930e00f3e65a2c5
Kafadar <kafadare@reslnvhpc0361.research.chop.edu>
2024-10-04 15:37:29 -0400

    1)Changed name in saved file from Twin to nonSingleton to reflect new variable. 2) Changed default files/folders to point to the matched TRAIN set and the allPhen output folders. 3) Changed how df is loaded to ensure that all rows with NA in age, nonSingleton and sex == I are not included in the df that is input to gamlss (instead of giving na.omit(df) to the gamlss model). Very few I, so does not make sense to include in the model. NA age is because of no gestAge data. NA nonSingleton is because no twin status data. 4) Changed how modelname is saved by saving dist family, phenotype, and whether nonSingleton is in the model.

--- abcd_gamlss_model_fit.R
+++ abcd_gamlss_model_fit.R
@@ -18,18 +18,25 @@ if (length(args) > 1) {
   data_filename <- args[3]
   out_folder <- args[4]
   output_file <- args[5]
+  #method <- args[6]
 }else{
-  models_folder <- "~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_models/"
-  data_filename <- "/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/CSV/process_tables_5.1/abcd5.1_long_selectVars_NOdxfilter_famfilter2024-07-15.csv"
-  out_folder <- "~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/"
-  output_file <- "~/Documents/Grad_School/BGDLab/ABCD_data/gamlss_fits/gamlss_fits_stats.csv"
-}
+  n = 1
+  models_folder <- "/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_allPhen/"
+  data_filename <- "/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/CSV/process_tables_5.1/abcd_baseline_matchedTrain_selectVars_famfilter2024-10-04.csv"
+  out_folder <- "/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_allPhen/"
+  output_file <- "/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_allPhen/gamlss_fits_stats_allPhen.csv"
+  #method <- quote(CG)
+  }
 
 #rename some of the variables and select baseline scans
-rename_vec <- c(age = "PCW_at_scan", sex = "sex_baseline", site = "site_id_l", twin = "twin_statusFAM")
+rename_vec <- c(age = "PCW_at_scan", sex = "sex_baseline", site = "site_id_l", nonSingleton = "nonSingleton")
 
-#read in the dataframe and set variable types
-df <- read.csv(data_filename) %>% select(-1) %>% rename(., all_of(rename_vec)) %>% subset(eventname == "baseline_year_1_arm_1")#9396 subjects!
+# #read in the dataframe and set variable types, subset for only baseline data
+#subset for age (PCW at scan) not being NA (will be NA is gestAge was NA, should be N = 66)
+#subset for nonSingleton not being NA(will be NA if the parent questionnaire was NA, should be N = 2)
+#subset for sex %in% M or F, so no I in the model (should be N = 3 for I)
+df <- read.csv(data_filename) %>% select(-1) %>% rename(., all_of(rename_vec)) %>% 
+  subset(eventname == "baseline_year_1_arm_1" & !is.na(age) & !is.na(nonSingleton) & (sex %in% c("M", "F")))#4512 folks
 df$sex <- as.factor(df$sex)
 df$site <- as.factor(df$site)
 df$age <- as.numeric(df$age)
@@ -40,37 +47,38 @@ model <- readRDS(models_list[n])
 #model_name <- paste(Reduce(paste, deparse(model$mu.formula)),model$fam, sep = "__")
 print(paste(Reduce(paste, deparse(model$mu.formula)),model$fam, sep = "__"))
 
-twin <- ifelse(sum(grepl("twin", as.character(model$mu.formula))) != 0, "TWIN", NA)
+nonSingleton <- ifelse(sum(grepl("nonSingleton", as.character(model$mu.formula))) != 0, "nonSingleton", NA)
 
 #select variables for gamlss, including phenotype specified by the loaded model
-df <- df %>% select(sex, age, site, twin, model$phenotype)
+df <- df %>% select(sex, age, site, nonSingleton, model$phenotype)
 
 #modelname <- paste("gamlss_FIT",sub(".*/gamlss_model_(.*)", "\\1", model_name), sep = "__")
-modelname <- paste("gamlssFIT", model$fam, model$phenotype, twin, sep = "__")
-filename <- paste0(out_folder,modelname)
+modelname <- paste("gamlssFIT", model$fam, model$phenotype, nonSingleton, sep = "__")
+filename <- paste0(out_folder,modelname,".rds")
 
 gamlss <- gamlss (formula = model$mu.formula,
-                            sigma.formula = model$sigma.formula,
-                            tau.formula = model$tau.formula,
-                            family = model$fam,
-                            data = na.omit(df),
-                            control = gamlss.control(n_cyc = model$n_crit))
-          
+                  sigma.formula = model$sigma.formula,
+                  tau.formula = model$tau.formula,
+                  family = model$fam,
+                  method = CG(),
+                  data = df,
+                  control = gamlss.control(n_cyc = model$n_crit))
+
 output <- data.frame(model = Reduce(paste, deparse(model$mu.formula)),
-                         family_abbr = gamlss$family[1],
-                         family = gamlss$family[2],
-                         logLik = logLik(gamlss), 
-                         AIC = AIC(gamlss), 
-                         BIC = BIC(gamlss),
-                         n_cyc = model$n_crit,
-                         data = basename(data_filename),
-                         warning = NA)
+                     family_abbr = gamlss$family[1],
+                     family = gamlss$family[2],
+                     method = as.character(gamlss$method),
+                     logLik = logLik(gamlss), 
+                     AIC = AIC(gamlss), 
+                     BIC = BIC(gamlss),
+                     n_cyc = model$n_crit,
+                     data = basename(data_filename),
+                     warning = NA)
 print(filename)
 saveRDS(gamlss, file = filename)
-    
+
 if (!file.exists(output_file)) {
-    write.table(output, file = output_file, sep = ",", row.names = FALSE, col.names = TRUE, append = FALSE)
-    } else {
-      write.table(output, file = output_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
+  write.table(output, file = output_file, sep = ",", row.names = FALSE, col.names = TRUE, append = FALSE)
+} else {
+  write.table(output, file = output_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
 }
-  
