76dce24be6890a692c3b9496d7c3ad5da2c98b02
Kafadar <kafadare@reslnvhpc0361.research.chop.edu>
2024-10-04 15:38:15 -0400

    Changed the pointers for files to point to the allPhen folder, should make this a function input in the future

--- array_script_submit.sh
+++ array_script_submit.sh
@@ -5,8 +5,12 @@ FOLDER_NAME=$2
 
 NO_FILES=$(ls -1 "$FOLDER_NAME"| wc -l)
 
+#NO_FILES=2
+
 sbatch --array=1-$NO_FILES "$SUBMIT_SCRIPT"
+
 
 
 #Submit with following command (customize...)
 #sbatch array_script_submit.sh submit_gamlss_fits_ABCD.sh /mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_family_v2
+#sbatch array_script_submit.sh submit_gamlss_fits_ABCD.sh /mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_allPhen
--- submit_gamlss_fits_ABCD.sh
+++ submit_gamlss_fits_ABCD.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 #SBATCH --job-name=ABCD_family_selection_test
-#SBATCH --output=/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/new_fits_singleton/out_messages_new/%x_%A_%a_output.txt
+#SBATCH --output=/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_allPhen/out_messages/%x_%A_%a_output.txt
 #SBATCH --time=04:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
 
 # Directory containing the model files
-MODEL_DIR="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_family_v2/"
-DATA_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/CSV/process_tables_5.1/abcd5.1_long_selectVars_NOdxfilter_famfilter2024-09-20.csv"
-OUT_FOLDER="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/new_fits_singleton/"
-OUTPUT_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_family_CG/new_fits_singleton/gamlss_family_fits_stats_CG_new.csv"
+MODEL_DIR="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_models_allPhen/"
+DATA_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/CSV/process_tables_5.1/abcd_baseline_matchedTrain_selectVars_famfilter2024-10-04.csv"
+OUT_FOLDER="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_allPhen/"
+OUTPUT_FILENAME="/mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/gamlss_fits_allPhen/gamlss_family_fits_stats_allPhen.csv"
+
+#Create a temp directory for the current job
+TMPDIR=$(mktemp -d /mnt/isilon/bgdlab-processing/Eren/job_${SLURM_ARRAY_TASK_ID}_XXXXXX)
+export TMPDIR
+cd $TMPDIR
 
 module load R/4.2.3
 
 # Run the R script with the specified model
-Rscript abcd_gamlss_model_fit_CG.R $SLURM_ARRAY_TASK_ID $MODEL_DIR $DATA_FILENAME $OUT_FOLDER $OUTPUT_FILENAME
+Rscript /mnt/isilon/bgdlab_processing/Eren/ABCD-braincharts/ABCD_premie_code/abcd_gamlss_model_fit.R $SLURM_ARRAY_TASK_ID $MODEL_DIR $DATA_FILENAME $OUT_FOLDER $OUTPUT_FILENAME
+
+trap "rm -rf $TMPDIR" EXIT
