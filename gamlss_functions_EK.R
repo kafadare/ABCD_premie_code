831fe1baa51495ddda97e7a70a5c1c8d896c144e
Kafadar <kafadare@reslnvhpc0361.research.chop.edu>
2024-12-19 04:43:45 -0500

    edited the function to 1)have a longitudinal model option, 2) save models with numbers instead of model specification -- easier to match with output and also no issues with too long file name

--- gamlss_functions_EK.R
+++ gamlss_functions_EK.R
@@ -15,26 +15,67 @@ generate_gamlss_models <- function(phenotype_set,
       family <- family_set[i]
         for (j in 1:length(phenotype_set)) {
           ph <- phenotype_set[j]
+          n = 0
           #if generating specifications for longitudinal models, add phenotype from t1 to the terms
           if (long == TRUE){
             ph.t1 <- ph %>% gsub(".t2", ".t1", .)
-            mu.terms <- c(mu_terms, ph.t1)
-            sigma.terms <- c(sigma_terms, ph.t1)
-            nu.terms <- c(nu_terms, ph.t1)
+            mu.terms <- c(mu_terms, ph.t1) %>% {c(head(., 1), tail(., 1), .[-c(1, length(.))])}
+            sigma.terms <- c(sigma_terms, ph.t1) %>% {c(head(., 1), tail(., 1), .[-c(1, length(.))])}
+            nu.terms <- c(nu_terms, ph.t1) %>% {c(head(., 1), tail(., 1), .[-c(1, length(.))])}
+          } else{
+            mu.terms <- mu_terms
+            sigma.terms <- sigma_terms
+            nu.terms <- nu_terms
           }
-          for (k in 1:length(mu.terms)) {
-            mu_model_string <- paste0(ph, paste(mu.terms[1:k], collapse = "+"))
+          #Generate combinations of terms
+          mu_combinations <- rbindlist(lapply(1:length(mu.terms), function(k) {
+            comb <- combn(mu.terms, k, simplify = FALSE)
+            data.frame(combination = sapply(comb, paste, collapse = " + "))  # Combine into a data frame
+          })) 
+          mu_combinations <- mu_combinations[grep("random(site)",mu_combinations$combination, fixed = TRUE),]
+          mu_combinations <- mu_combinations[grep("~1",mu_combinations$combination, fixed = TRUE),]
+          if (long == TRUE){
+            mu_combinations <- mu_combinations[grep(ph.t1,mu_combinations$combination, fixed = TRUE),]
+          }
+          #Generate combinations of terms
+          sigma_combinations <- rbindlist(lapply(1:length(sigma.terms), function(k) {
+            comb <- combn(sigma.terms, k, simplify = FALSE)
+            data.frame(combination = sapply(comb, paste, collapse = " + "))  # Combine into a data frame
+          })) 
+          #sigma_combinations <- sigma_combinations[grep("random(site)",sigma_combinations$combination, fixed = TRUE),]
+          sigma_combinations <- sigma_combinations[grep("~1",sigma_combinations$combination, fixed = TRUE),]
+          # if (long == TRUE){
+          #   sigma_combinations <- sigma_combinations[grep(ph.t1,sigma_combinations$combination, fixed = TRUE),]
+          # }
+          #Generate combinations of terms
+          nu_combinations <- rbindlist(lapply(1:length(nu.terms), function(k) {
+            comb <- combn(nu.terms, k, simplify = FALSE)
+            data.frame(combination = sapply(comb, paste, collapse = " + "))  # Combine into a data frame
+          })) 
+          #nu_combinations <- nu_combinations[grep("random(site)",nu_combinations$combination, fixed = TRUE),]
+          nu_combinations <- nu_combinations[grep("~1",nu_combinations$combination, fixed = TRUE),]
+          # if (long == TRUE){
+          #   nu_combinations <- nu_combinations[grep(ph.t1,nu_combinations$combination, fixed = TRUE),]
+          # }
+          
+          for (k in 1:dim(mu_combinations)[1]) {
+            mu_model_string <- paste0(ph, as.character(mu_combinations[k, "combination"]))
             
-            for (l in 1:length(sigma.terms)) {
-              if(l>k){
-                l = k
+            for (l in 1:dim(sigma_combinations)[1]) {
+              sigma_model_string <- paste0(ph, as.character(sigma_combinations[l, "combination"]))
+              if(!all(strsplit(sigma_model_string, " \\+ ")[[1]] %in% strsplit(mu_model_string, " \\+ ")[[1]])){
+                #print(mu_model_string)
+                #print(sigma_model_string)
+                break
               }
-              sigma_model_string <- paste0(ph, paste(sigma.terms[1:l], collapse = "+"))
-              for (m in 1: length(nu.terms)){
-                if(m>l){
-                  m = l
+              
+              for (m in 1: dim(nu_combinations)[1]){
+                nu_model_string <- paste0(ph,as.character(nu_combinations[m, "combination"]))
+                if(!all(strsplit(nu_model_string, " \\+ ")[[1]] %in% strsplit(sigma_model_string, " \\+ ")[[1]])){
+                  #print(sigma_model_string)
+                  #print(nu_model_string)
+                  break
                 }
-                nu_model_string <- paste0(ph, paste(nu.terms[1:m], collapse = "+"))
                 #check if fourth moment, tau has terms
                 if (!is.null(tau_terms)){
                   for (n in 1: length(nu.terms)){
@@ -82,38 +123,21 @@ generate_gamlss_models <- function(phenotype_set,
                     phenotype = ph)
                   #if generating specifications for longitudinal models, change the name of the term to "t1"
                   #to avoid "file name too long" error
-                  if(long == TRUE){
-                    mu.names <- mu.terms %>% sub(".*\\.t1$", "t1", .)
-                    sigma.names <- sigma.terms %>% sub(".*\\.t1$", "t1", .)
-                    nu.names <- nu.terms %>% sub(".*\\.t1$", "t1", .)
-                  }
+                  # if(long == TRUE){
+                  #   mu.names <- mu_combinations$combination %>% sub(" [^ ]*\\.t1", " t1", .)
+                  #   sigma.names <- sigma_combinations$combination %>% sub(" [^ ]*\\.t1", " t1", .)
+                  #   nu.names <- nu_combinations$combination %>% sub(" [^ ]*\\.t1", " t1", .)
+                  # }
                   #define model name to save
-                  modelname <- paste("gamlss_model",
-                                     "MU", paste(mu.names[1:k], collapse = "."),
-                                     "SIGMA", paste(sigma.names[1:l], collapse = "."),
-                                     "NU", paste(nu.names[1:m], collapse = "."), 
+                  n = n + 1
+                  modelname <- paste("gamlss_model",n, 
                                      family_set[i],
                                      phenotype_set[j], sep = "_")
-                  #do not save model if there is no random(site) effect in the mu term
-                  #do not save if generating longitudinal models and there is no t1 phenotype in MU
-                  if(long == TRUE){
-                    if(grepl("MU.*\\.t1.*SIGMA", modelname)){
-                      if(grepl("MU.*site.*SIGMA", modelname)){
-                        print("saving longitudinal models")
-                        filename <- paste0(modelname,".rds")
-                        saveRDS(model_specs, file = filename)
-                        rm(filename)
-                      }
-                    }
-                  } else if (long == FALSE){
-                    if(grepl("MU.*site.*SIGMA", modelname)){
-                      print("saving non-longitudinal models")
+
+                      print("saving models")
                       filename <- paste0(modelname,".rds")
                       saveRDS(model_specs, file = filename)
-                      rm(filename)
-                    }
-                  }
-                  rm(model_specs, modelname)
+                      rm(filename,model_specs, modelname)
             }#end of else
            }#end of loop m
          }#end of loop l
