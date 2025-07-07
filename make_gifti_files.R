55f29bd32d1b61e54abc0ece314ae8704ac0d5c4
kafadare <kafadare@pennmedicine.upenn.edu>
2025-07-07 22:58:35 +0300

    new model selection for all models

--- make_gifti_files.R
+++ make_gifti_files.R
@@ -17,8 +17,10 @@ bw_betas <- read.csv('~/Documents/Grad_School/BGDLab/ABCD_data/dsk_birthweight_a
 bw_betas_left <- bw_betas %>% filter(hemisphere == "L")
 bw_betas_right <- bw_betas %>% filter(hemisphere == "R")
 bw_pgs_betas <- read.csv('~/Documents/Grad_School/BGDLab/ABCD_data/dsk_birthweightPGS_all.csv')
-bw_pgs_betas_left <- bw_betas %>% filter(hemisphere == "L")
-bw_pgs_betas_right <- bw_betas %>% filter(hemisphere == "R")
+bw_pgs_betas_left <- bw_pgs_betas %>% filter(hemisphere == "L")
+bw_pgs_betas_right <- bw_pgs_betas %>% filter(hemisphere == "R")
+
+
 
 #Add a column to BW and BW-pgs beta csv files to correspond to the numerical labels
 left_labels <- as.data.frame(atlas_lh$label) %>% mutate(label = rownames(.))
@@ -26,8 +28,26 @@ right_labels <- as.data.frame(atlas_rh$label) %>% mutate(label = rownames(.))
 bw_betas_left <- merge(bw_betas_left, left_labels[,c("label", "Key")], by = "label")
 bw_betas_right <- merge(bw_betas_right, right_labels[,c("label", "Key")], by = "label")
 
-bw_pgs_betas_left <- merge(bw_betas_left, left_labels[,c("label", "Key")], by = "label")
-bw_pgs_betas_right <- merge(bw_betas_right, right_labels[,c("label", "Key")], by = "label")
+bw_pgs_betas_left <- merge(bw_pgs_betas_left, left_labels[,c("label", "Key")], by = "label")
+bw_pgs_betas_right <- merge(bw_pgs_betas_right, right_labels[,c("label", "Key")], by = "label")
+
+#Order the betas tables by the key order
+bw_betas_left <-  bw_betas_left[order(as.numeric(bw_betas_left$Key)), ]
+bw_betas_right <- bw_betas_right[order(as.numeric(bw_betas_right$Key)), ]
+
+bw_pgs_betas_left <- bw_pgs_betas_left[order(as.numeric(bw_pgs_betas_left$Key)), ]
+bw_pgs_betas_right <- bw_pgs_betas_right[order(as.numeric(bw_pgs_betas_right$Key)), ]
+
+#combine csvs to have a single one including both hemispheres (no subcortical structures)
+bw_betas_save <- rbind(bw_betas_left, bw_betas_right)
+bw_pgs_betas_save <- rbind(bw_pgs_betas_left, bw_pgs_betas_right)
+#save the ordered csvs
+write.csv(bw_betas_save, "~/Documents/Grad_School/BGDLab/ABCD_data/bw_betas_ordered.csv")
+write.csv(bw_pgs_betas_save, "~/Documents/Grad_School/BGDLab/ABCD_data/bw_pgs_betas_ordered.csv")
+#save single text file of the ordered beta weights
+write.table(bw_betas_save$bw_beta, file = "~/Documents/Grad_School/BGDLab/ABCD_data/bw_betas.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(bw_pgs_betas_save$bw_pgs_beta, file = "~/Documents/Grad_School/BGDLab/ABCD_data/bw_pgs_betas.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
 
 #From Kevin Sun's code
 #define BW and BW-pgs maps using dsk atlas for lh and rh
